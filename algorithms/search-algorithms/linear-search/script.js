14927581240f80560f669a93a639eea1d09d49b7
Niels Lange <info@nielslange.de>
2021-01-24 15:14:10 +0700

    👌 IMPROVE: Format script.js

--- algorithms/search-algorithms/linear-search/script.js
+++ algorithms/search-algorithms/linear-search/script.js
@@ -6,10 +6,10 @@
  * @return {number?} The index of the wanted value or null on failure.
  */
 function linearSearch( haystack, key ) {
-	for ( let index = 0; index < haystack.length; index++ ) {
-		if ( key === haystack[ index ] ) return index;
-	}
-	return null;
+  for ( let index = 0; index < haystack.length; index++ ) {
+    if ( key === haystack[ index ] ) return index;
+  }
+  return null;
 }
 
 const data = [ 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 ];
