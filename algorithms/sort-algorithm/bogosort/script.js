0d9967fc7e7e5052e208924c4b3c127cb374ccab
Niels Lange <info@nielslange.de>
2021-01-28 17:54:53 +0700

    👌 IMPROVE: Adjust Bogo Sort

--- algorithms/sort-algorithm/bogosort/script.js
+++ algorithms/sort-algorithm/bogosort/script.js
@@ -1,16 +1,16 @@
 /**
- * Bogosort
+ * Bogo Sort
  *
  * @param {array} array The original array.
  * @return {array} The sorted array.
  */
 function bogoSort( array ) {
-  for ( let index = 0; index < array.length - 1; index++ ) {
-    if ( array[ index ] > array[ index + 1 ] ) {
-      return bogoSort( shuffle( array ) );
-    }
-  }
-  return array;
+	for ( let index = 0; index < array.length - 1; index++ ) {
+		if ( array[ index ] > array[ index + 1 ] ) {
+			return bogoSort( shuffle( array ) );
+		}
+	}
+	return array;
 }
 
 /**
@@ -21,12 +21,12 @@ function bogoSort( array ) {
  * @see https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
  */
 function shuffle( array ) {
-  for ( let i = array.length - 1; i > 0; i-- ) {
-    let j = Math.floor( Math.random() * ( i + 1 ) );
-    [ array[ i ], array[ j ] ] = [ array[ j ], array[ i ] ];
-  }
+	for ( let i = array.length - 1; i > 0; i-- ) {
+		let j = Math.floor( Math.random() * ( i + 1 ) );
+		[ array[ i ], array[ j ] ] = [ array[ j ], array[ i ] ];
+	}
 
-  return array;
+	return array;
 }
 
 const data = [ 2, 3, 6, 5, 4, 1 ];
--- algorithms/sort-algorithm/bogosort/script.php
+++ algorithms/sort-algorithm/bogosort/script.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Bogosort
+ * Bogo Sort
  *
  * @param {array} $array The original array.
  * @return {array} The sorted array.
  */
 function bogoSort( $array ) {
-  for ( $index = 0; $index < $array.length - 1; $index++ ) {
+  for ( $index = 0; $index < count( $array ) - 1; $index++ ) {
     if ( $array[ $index ] > $array[ $index + 1 ] ) {
-      return bogoSort( shuffle( $array ) );
+      shuffle( $array );
+      return bogoSort( $array );
     }
   }
   return $array;
@@ -20,4 +21,6 @@ $start  = microtime( true );
 $result = bogoSort( $data );
 printf( 'Total execution time in seconds: %s', microtime( true ) - $start );
 
-print( $result );
+print( '<pre>' );
+print_r( $result );
+print( '</pre>' );
